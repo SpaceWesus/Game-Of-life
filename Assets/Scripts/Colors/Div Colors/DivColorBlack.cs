eab94d70b08986b47c3185dc0c2bcc1b08c16b14
swooshybop <dparikh8@asu.edu>
2025-11-06 10:34:52 -0800

    Added my color purple
    If all 4 corner are different colors it spawns, if neighbour is not black or purple DIES!!....

---
+++ Assets/Scripts/Colors/Div Colors/DivColorBlack.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class DivColorBlack : Donovan_ColorBlack
+{
+    public Color colorPurple;
+
+    public override Color CheckRules(Vector2Int pos)
+    {
+        Grid grid = transform.parent.GetComponent<Grid>();
+        Color[] colorList = new Color[1];
+        Color[] colorList2 = new Color[1];
+
+        // If exactly 3 yellow corner neighbors, turn yellow.
+        colorList[0] = colorYellow;
+        if (grid.CheckCount(pos, colorList, 3, Grid.Neighbors.Corner, Grid.Operation.Equal)) return colorYellow;
+
+        // If exactly 3 red adjacent neighbors, turn red.
+        colorList[0] = colorRed;
+        if (grid.CheckCount(pos, colorList, 3, Grid.Neighbors.Adjacent, Grid.Operation.Equal)) { Debug.Log("RED BORN"); return colorRed; }
+
+        // If exactly 3 white neighbors, turn white.
+        colorList[0] = colorWhite;
+        if (grid.CheckCount(pos, colorList, 3, Grid.Neighbors.All, Grid.Operation.Equal)) return colorWhite;
+
+        // If exactly 3 non-Black or Blue neighbors (5 Black or Blue neighbors) and at least one is Green, turn Green.
+        colorList = new Color[2];
+        colorList[0] = color;
+        colorList[1] = colorBlue;
+        colorList2[0] = colorGreen;
+        if (grid.CheckCount(pos, colorList, 5, Grid.Neighbors.All, Grid.Operation.Equal) && grid.CheckCount(pos, colorList2, 1, Grid.Neighbors.All, Grid.Operation.GreaterThanEqual)) return colorGreen;
+
+        //if black has 1 purple adjacent, and the rest black, it grows
+        colorList[0] = colorPurple;
+
+        Color[] blackList = new Color[1] { color };
+        if (grid.CheckCount(pos, colorList, 1, Grid.Neighbors.Adjacent, Grid.Operation.Equal) &&
+        grid.CheckCount(pos, blackList, 7, Grid.Neighbors.All, Grid.Operation.Equal))
+        {
+            return colorPurple;
+        }
+
+        //if all 4 corners have a different color (excluding black, turn purple)
+        Color corner1 = grid.GetColorAtPos(new Vector2Int(pos.x - 1, pos.y + 1));
+        Color corner2 = grid.GetColorAtPos(new Vector2Int(pos.x + 1, pos.y + 1));
+        Color corner3 = grid.GetColorAtPos(new Vector2Int(pos.x - 1, pos.y - 1));
+        Color corner4 = grid.GetColorAtPos(new Vector2Int(pos.x + 1, pos.y - 1));
+
+        //no black corner
+        if (corner1 != color && corner2 != color && corner3 != color && corner4 != color)
+        {
+            //all corners are different colors
+            if (corner1 != corner2 && corner1 != corner3 && corner1 != corner4 &&
+            corner2 != corner3 && corner2 != corner4 &&
+            corner3 != corner4)
+            {
+                return colorPurple;
+            }
+
+        }
+
+        // Otherwise turn black.
+        return color;
+    }
+}
---
+++ Assets/Scripts/Colors/Div Colors/DivColorPurple.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class DivColorPurple : ColorScript
+{
+    public Color colorBlack;
+
+    public override Color CheckRules(Vector2Int pos)
+    {
+
+        Grid grid = transform.parent.GetComponent<Grid>();
+
+        Color[] safeColors = new Color[2];
+        safeColors[0] = colorBlack;
+        safeColors[1] = color;
+
+
+        //if any of its neighbour is not black or itself, dies
+        if (grid.CheckCount(pos, safeColors, 8, Grid.Neighbors.All, Grid.Operation.LessThan))
+        {
+            return colorBlack;
+        }
+
+        return color;
+    }
+
+    void Start()
+    {
+        //ebug.LogWarning("--- My name is " + gameObject.name + " and my color in the Inspector is " + color + " ---");
+    }
+}
